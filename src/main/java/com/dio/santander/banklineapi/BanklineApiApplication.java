e1b6c387713f6805e0f3ba3c78e8a7a15dc1f410
Jean Machado <jeannzk021@gmail.com>
2022-05-30 18:05:59 -0300

    Finalização projeto - Api JAVA

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -1,5 +1,4 @@
 package com.dio.santander.banklineapi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/dio/santander/banklineapi/controllers/MovimentacaoController.java
@@ -0,0 +1,39 @@
+package com.dio.santander.banklineapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.model.Movimentacao;
+import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
+import com.dio.santander.banklineapi.service.MovimentacaoService;
+
+@RestController
+@RequestMapping("/movimentacoes")
+public class MovimentacaoController {
+	@Autowired
+	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private MovimentacaoService service;
+	
+	
+	@GetMapping
+	public List<Movimentacao> findAll() {
+		return repository.findAll();
+		
+	}
+	
+	@PostMapping
+	public void save(@RequestBody NovaMovimentacao movimentacao) {
+		service.save(movimentacao);
+		
+	}
+
+}
